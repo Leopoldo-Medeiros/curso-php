fda0fce91033d7cfe372222c08177f4e83a2aca1
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-01-10 17:05:08 +0000

    First commit

---
+++ array/basico.php
@@ -0,0 +1,25 @@
+<div class="titulo">Array</div>
+
+<?php
+$lista = array(1, 2, 3.4, "texto");
+echo $lista . '<br>';
+var_dump($lista);
+echo '<br>';
+print_r($lista);
+echo '<br>';
+
+$lista[0] = 5432;
+print_r($lista);
+
+echo '<br>' . $lista[0];
+echo '<br>' . $lista[1];
+echo '<br>' . $lista[2];
+echo '<br>' . $lista[3];
+echo '<br>';
+var_dump($lista[4]);
+
+$texto = 'Leopoldo Ferreira Medeiros';
+echo '<br>' . $texto[0];
+echo '<br>' . $texto[1];
+echo '<br>' . $texto[11]; // Cuidado!
+echo '<br>' . mb_strstr($texto, 10, 3);
