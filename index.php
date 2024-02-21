71cd1d42d870e58b74943218657f766ebb2425a6
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-02-21 01:15:06 +0000

    WIP

--- index.php
+++ index.php
@@ -22,6 +22,16 @@
                             Laço For
                         </a>
                     </li>
+                    <li>
+                        <a href="exercicio.php?dir=repeticoes&file=desafio_for">
+                            Desafio For
+                        </a>
+                    </li>
+                    <li>
+                        <a href="exercicio.php?dir=repeticoes&file=foreach">
+                            Foreach
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div class="modulo laranja">
