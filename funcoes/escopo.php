f8dd6ade0872d2c028df4369d677372ff203b312
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-03-24 19:38:57 +0000

    Frontend Fix

---
+++ funcoes/escopo.php
@@ -0,0 +1,8 @@
+<?php
+include "../includes/header.php";
+?>
+    <main class="principal">
+        <div class="conteudo">
+            <div class="titulo">Função & Escopo</div>
+            <hr>
+<?php
