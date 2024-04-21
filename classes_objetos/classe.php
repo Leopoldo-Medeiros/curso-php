fc4afe00e1e43e381819f95f46d5602d54c9c89e
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-04-21 16:28:38 +0100

    WIP - Classe

---
+++ classes_objetos/classe.php
@@ -0,0 +1,33 @@
+<?php
+include "../includes/header.php";
+?>
+    <main class="principal">
+        <div class="conteudo">
+            <div class="titulo">Primeira Classe</div>
+            <hr>
+
+<?php
+
+class Cliente
+{
+    // Atributos
+    public $nome = "Pedro";
+    public $idade = 20;
+
+    public function show()
+    {
+        return "Nome: {$this->nome} Idade: {$this->idade}";
+    }
+}
+$c1 = new Cliente();
+echo $c1->nome, '<br>';
+$c1->nome = 'Leo Medeiros';
+echo $c1->nome, '<br>';
+$c1->idade = 20;
+
+$c2 = New Cliente();
+$c2->nome = 'Luciana';
+$c2->idade = 25;
+
+echo $c1->show(), '<br>';
+echo $c2->show();
